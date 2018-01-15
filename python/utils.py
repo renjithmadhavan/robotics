3f874b7c492e14d02e8c9c7e7cd19cef1de5f192
Renjith Madhavan <renjithmadhavan@gmail.com>
2018-01-15 17:21:10 -0600

    Update utils.py

--- python/utils.py
+++ python/utils.py
@@ -1,3 +1,4 @@
+import RPi.GPIO as gpio
 import picamera
 import time
 
@@ -15,3 +16,64 @@ import time
 ## camera.stop_recording()
 
 
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(7, gpio.OUT)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(13, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+
+def forward(tf):
+    init()
+    gpio.output(13, True)
+    gpio.output(15, False)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+def reverse(tf):
+    init()
+    gpio.output(13, False)
+    gpio.output(15, True)
+    gpio.output(7, True)
+    gpio.output(11, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_left(tf):
+    init()
+    gpio.output(7, True)
+    gpio.output(11, True)
+    gpio.output(13, True)
+    gpio.output(15, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def turn_right(tf):
+    init()
+    gpio.output(7, False)
+    gpio.output(13, False)
+    gpio.output(15, False)
+    gpio.output(11, True)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+def pivot_left(tf):
+    init()
+    gpio.output(13, True)
+    gpio.output(15, False)
+    gpio.output(7, True)
+    gpio.output(11, False)
+    time.sleep(tf)
+    gpio.cleanup()
+    
+
+def pivot_right(tf):
+    init()
+    gpio.output(13, False)
+    gpio.output(15, True)
+    gpio.output(7, False)
+    gpio.output(11, True)
+    time.sleep(tf)
+    gpio.cleanup()
