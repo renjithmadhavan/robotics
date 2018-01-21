e5bac81b63f74a314bcabac65651810c9b1613f7
renjithmadhavan <renjithmadhavan@gmail.com>
2018-01-20 19:29:43 -0800

    c1

--- python/distance.py
+++ python/distance.py
@@ -4,14 +4,14 @@ import time
 def distance(measure='cm'):
     try:
         gpio.setmode(gpio.BOARD)
-        gpio.setup(12, gpio.OUT)
-        gpio.setup(22, gpio.IN)
+        gpio.setup(18, gpio.OUT)
+        gpio.setup(25, gpio.IN)
         
-        gpio.output(12, False)
-        while gpio.input(22) == 0:
+        gpio.output(18, False)
+        while gpio.input(25) == 0:
             nosig = time.time()
 
-        while gpio.input(22) == 1:
+        while gpio.input(25) == 1:
             sig = time.time()
 
         tl = sig - nosig
