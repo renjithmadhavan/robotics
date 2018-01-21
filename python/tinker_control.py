9046e0d16ccb9a3ff25921340257972a9822a2c0
renjithmadhavan <renjithmadhavan@gmail.com>
2018-01-20 19:35:27 -0600

    t1

--- python/tinker_control.py
+++ python/tinker_control.py
@@ -5,65 +5,65 @@ import time
 import Tkinter as tk
 
 def init():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+    gpio.setup(16, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
 
 def forward(tf):
     #init()
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, False)
-    gpio.output(24, True)
+    gpio.output(11, True)
+    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, True)
     time.sleep(tf)
     gpio.cleanup()
 
 def reverse(tf):
     #init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
+    gpio.output(11, False)
+    gpio.output(15, True)
+    gpio.output(16, True)
+    gpio.output(18, False)
     time.sleep(tf)
     gpio.cleanup()
 
-def turn_left(tf):
+def goleft(tf):
     #init()
-    gpio.output(17, False)
-    gpio.output(22, False)
-    gpio.output(23, False)
-    gpio.output(24, True)
+    gpio.output(11, False)
+    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, True)
     time.sleep(tf)
     gpio.cleanup()
 
-def turn_right(tf):
+def goright(tf):
     #init()
-    gpio.output(17, True)
-    gpio.output(22, True)
-    gpio.output(23, True)
-    gpio.output(24, False)
+    gpio.output(11, True)
+    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, False)
     time.sleep(tf)
     gpio.cleanup()
 
 
 def pivot_left(tf):
     #init()
-    gpio.output(13, True)
+    gpio.output(11, True)
     gpio.output(15, False)
-    gpio.output(7, True)
-    gpio.output(11, False)
+    gpio.output(16, True)
+    gpio.output(18, False)
     time.sleep(tf)
     gpio.cleanup()
 
 
 def pivot_right(tf):
     #init()
-    gpio.output(13, False)
+    gpio.output(11, False)
     gpio.output(15, True)
-    gpio.output(7, False)
-    gpio.output(11, True)
+    gpio.output(16, False)
+    gpio.output(18, True)
     time.sleep(tf)
     gpio.cleanup()
 
@@ -79,9 +79,9 @@ def key_input(event):
     elif key_press.lower() == 's':
         reverse(sleep_time)
     elif key_press.lower() == 'a':
-        turn_left(sleep_time)
+        goleft(sleep_time)
     elif key_press.lower() == 'd':
-        turn_right(sleep_time)
+        goright(sleep_time)
     elif key_press.lower() =='q':
         pivot_left(sleep_time)
     elif key_press.lower() == 'e':
--- python/utils.py
+++ python/utils.py
@@ -17,63 +17,62 @@ import time
 
 
 def init():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(15, gpio.OUT)
+    gpio.setup(16, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
 
 def forward(tf):
     init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, False)
-    gpio.output(24, True)
+    gpio.output(11, True)
+    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, True)
     time.sleep(tf)
     gpio.cleanup()
-    
+
 def reverse(tf):
     init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
+    gpio.output(11, False)
+    gpio.output(15, True)
+    gpio.output(16, True)
+    gpio.output(18, False)
     time.sleep(tf)
     gpio.cleanup()
 
-def turn_left(tf):
+def goleft(tf):
     init()
-    gpio.output(17, True)
-    gpio.output(22, True)
-    gpio.output(23, True)
-    gpio.output(24, False)
+    gpio.output(11, False)
+    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, True)
     time.sleep(tf)
     gpio.cleanup()
 
-def turn_right(tf):
+def goright(tf):
     init()
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, True)
-    gpio.output(24, True)
+    gpio.output(11, True)
+    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, False)
     time.sleep(tf)
     gpio.cleanup()
- 
-def pivot_left(tf):
-    init()
-    gpio.output(17, True)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, False)
-    time.sleep(tf)
-    gpio.cleanup()
-    
 
-def pivot_right(tf):
-    init()
-    gpio.output(17, False)
-    gpio.output(22, True)
-    gpio.output(23, False)
-    gpio.output(24, True)
+def pivotleft(tf):
+    #init()
+    gpio.output(11, True)
+    gpio.output(15, False)
+    gpio.output(16, True)
+    gpio.output(18, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def pivotright(tf):
+    #init()
+    gpio.output(11, False)
+    gpio.output(15, True)
+    gpio.output(16, False)
+    gpio.output(18, True)
     time.sleep(tf)
     gpio.cleanup()
